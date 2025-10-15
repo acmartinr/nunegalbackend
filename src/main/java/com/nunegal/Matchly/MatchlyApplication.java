b59d204e15699fb39dbf2534541cc65f73b0be10
acmartinr <albertocarlosmartin@gmail.com>
2025-10-15 17:34:40 +0200

    aded project files

---
+++ src/main/java/com/nunegal/Matchly/MatchlyApplication.java
@@ -0,0 +1,13 @@
+package com.nunegal.Matchly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nunegal/Matchly/MatchlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunegal.Matchly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
